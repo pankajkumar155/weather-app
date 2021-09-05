1c3f56226ad51315805cf2414f62ea39ecd5229c
pankajkumar155 <87750349+pankajkumar155@users.noreply.github.com>
2021-09-05 22:11:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,61 +1,67 @@
-const weatherApi={
-    key:"35514ba957fbdb22b823a98d0da17e53",
-    baseUrl:"https://api.openweathermap.org/data/2.5/weather",
+const api = {
+    key: "35514ba957fbdb22b823a98d0da17e53",
+    base: "https://api.openweathermap.org/data/2.5/"
 }
-const searchInputBox=document.getElementById('input');
- searchInputBox.addEventListener('keypress', (event) => {
-     if(event.keyCode == 13){
-         console.log(searchInputBox.value);
-         getWeatherReport(searchInputBox.value);
-     }
-});
-function getWeatherReport(city){
-    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
-    .then(weather => {
-        return weather.json();
-    }).then(showWeatherReport);
-}
-function showWeatherReport(weather){
-    console.log(weather);
-    let city=document.getElementById('city');
-    city.innerText = `${weather.name}, ${weather.sys.country}`;
 
-    let temp=document.getElementById('temp');
-    temp.innerHTML=`${Math.round(weather.main.temp)}&deg;C`;
+const search = document.querySelector(".search");
+const btn = document.querySelector(".btn");
+btn.addEventListener("click", getInput);
 
-    let x = document.getElementById('min-max');
-    x.innerHTML=`${Math.floor(weather.main.temp_min)}&degC (min) / ${Math.ceil(weather.main.temp_max)}&degC (max)`;
-
-    let weatherType=document.getElementById('weather');
-    weatherType.innerText=`${weather.weather[0].main}`;
-
-    let Tdate = document.getElementById('date');
-    let todayDate=new Date();
-    Tdate.innerText=manageDate(todayDate);
-
-    if(weatherType.textContent=='Clear'){
-        document.body.style.backgroundImage = "url('./img/clear_weather.jpg')"
-    }
-    else if(weatherType.textContent=='Clouds'){
-        document.body.style.backgroundImage = "url('./img/cloudy_weather.jpg')"
-    }
-    else if(weatherType.textContent=='Rain'){
-        document.body.style.backgroundImage = "url('./img/rainy.png')"
-    }
-    else if(weatherType.textContent=='Haze'){
-        document.body.style.backgroundImage = "url('./img/haze.jpg')"
-    }
-    else if(weatherType.textContent=='Thunderstorm'){
-        document.body.style.backgroundImage = "url('./img/thunderstorm_weather.jpg')"
+function getInput (event) {
+    event.preventDefault();
+    if (event.type == "click") {
+        getData(search.value);
+        console.log(search.value);
     }
 }
-function manageDate(a){
-    let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let months=["January","February","March","April","May","June","July","August","September","October","November","December"];
-    let year=a.getFullYear();
-    let month=months[a.getMonth()];
-    let day=days[a.getDay()];
-    let date=a.getDate();
 
-    return `${date} ${month} (${day}), ${year}`;
+function getData () {
+    fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
+        .then(response => {
+            return response.json();
+        }).then(displayData);
+        
+}
+
+function displayData (response) {
+    // console.log(response);
+    if (response.cod === "404") {
+        const error = document.querySelector(".error");
+        error.textContent = "Please enter a valid city";
+        search.value = "";
+    } else {
+        const city = document.querySelector(".city");
+        city.innerText = `${response.name}, ${response.sys.country}`;
+
+        const today = new Date();
+        const date = document.querySelector(".date");
+        date.innerText = dateFunction(today);
+
+        const temp = document.querySelector(".temp");
+        temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
+
+        const weather = document.querySelector(".weather");
+        weather.innerText = `Weather: ${response.weather[0].main}`;
+
+        const tempRange = document.querySelector(".temp-range");
+        tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C / ${Math.round(response.main.temp_max)}°C`;
+
+        const weatherIcon = document.querySelector(".weather-icon");
+        const iconURL = "http://openweathermap.org/img/w/";
+        weatherIcon.src = iconURL + response.weather[0].icon + ".png";
+
+        search.value = "";
+    }
+}
+
+function dateFunction (d) {
+    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day}, ${date} ${month} ${year}`;
 }
