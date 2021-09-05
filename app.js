b1ab5fea03f97d599373ecc156a523231fdc4ab0
pankajkumar155 <87750349+pankajkumar155@users.noreply.github.com>
2021-09-05 22:24:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,83 @@
-const api = {
+const weatherApi= {
     key: "35514ba957fbdb22b823a98d0da17e53",
-    base: "https://api.openweathermap.org/data/2.5/"
+    baseUrl: "https://api.openweathermap.org/data/2.5/weather",
 }
 
-const search = document.querySelector(".search");
-const btn = document.querySelector(".btn");
-btn.addEventListener("click", getInput);
-
-function getInput (event) {
-    event.preventDefault();
-    if (event.type == "click") {
-        getData(search.value);
-        console.log(search.value);
+const searchInputBox = document.getElementById('inputBox');
+searchInputBox.addEventListener('keypress',(event)=>{
+    if(event.keyCode == 13){
+        console.log(searchInputBox.value);
+        getWeatherReport(searchInputBox.value);
+        document.querySelector('.weatherBody').style.display = "block";
     }
+});
+
+function getWeatherReport(city){
+    fetch(`${weatherApi.baseUrl}?q=${city}&appid=${weatherApi.key}&units=metric`)
+    .then(weather => {
+        return weather.json();
+    }).then(showWeatherReport)
+    .catch(error=>{
+        alert('City Not Found');
+    });
 }
 
-function getData () {
-    fetch(`${api.base}weather?q=${search.value}&units=metric&appid=${api.key}`)
-        .then(response => {
-            return response.json();
-        }).then(displayData);
+function showWeatherReport(weather){
+    let city=document.getElementById('city');
+    city.innerText=`${weather.name},${weather.sys.country}`;
+
+    let temperature = document.getElementById('temp');
+    temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
+
+    let minMaxTemp = document.getElementById('min-max');
+    minMaxTemp.innerHTML = `${Math.floor(weather.main.temp_min)}&deg;C (min)/ ${Math.ceil(weather.main.temp_max)}&deg;C (max) `;
+
+    let weatherType = document.getElementById('weather');
+    weatherType.innerText = `${weather.weather[0].main}`;
+
+    let date = document.getElementById('date');
+    let todayDate = new Date();
+    date.innerText = dateManage(todayDate);
+
+    if(weatherType.textContent == 'Clear') {
+        document.body.style.backgroundImage = "url('clear.jpg')";
         
+    } else if(weatherType.textContent == 'Clouds') {
+
+        document.body.style.backgroundImage = "url('cloud.jpg')";
+        
+    } else if(weatherType.textContent == 'Haze') {
+
+        document.body.style.backgroundImage = "url('haze.jpg')";
+        
+    }     else if(weatherType.textContent == 'Rain') {
+        
+        document.body.style.backgroundImage = "url('rain.jpg')";
+        
+    } else if(weatherType.textContent == 'Snow') {
+        
+        document.body.style.backgroundImage = "url('snow.jpg')";
+    
+    } else if(weatherType.textContent == 'Thunderstorm') {
+    
+        document.body.style.backgroundImage = "url('thunder.jpg')";
+        
+    } else if(weatherType.textContent == 'Mist') {
+    
+        document.body.style.backgroundImage = "url('mist.jpg')";
+        
+    } 
 }
+function dateManage(dateArg) {
 
-function displayData (response) {
-    // console.log(response);
-    if (response.cod === "404") {
-        const error = document.querySelector(".error");
-        error.textContent = "Please enter a valid city";
-        search.value = "";
-    } else {
-        const city = document.querySelector(".city");
-        city.innerText = `${response.name}, ${response.sys.country}`;
-
-        const today = new Date();
-        const date = document.querySelector(".date");
-        date.innerText = dateFunction(today);
-
-        const temp = document.querySelector(".temp");
-        temp.innerHTML = `Temp: ${Math.round(response.main.temp)} <span>°C</span>`;
-
-        const weather = document.querySelector(".weather");
-        weather.innerText = `Weather: ${response.weather[0].main}`;
-
-        const tempRange = document.querySelector(".temp-range");
-        tempRange.innerText = `Temp Range: ${Math.round(response.main.temp_min)}°C / ${Math.round(response.main.temp_max)}°C`;
-
-        const weatherIcon = document.querySelector(".weather-icon");
-        const iconURL = "http://openweathermap.org/img/w/";
-        weatherIcon.src = iconURL + response.weather[0].icon + ".png";
-
-        search.value = "";
-    }
-}
-
-function dateFunction (d) {
-    let months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-    let day = days[d.getDay()];
-    let date = d.getDate();
-    let month = months[d.getMonth()];
-    let year = d.getFullYear();
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-    return `${day}, ${date} ${month} ${year}`;
+    let year = dateArg.getFullYear();
+    let month = months[dateArg.getMonth()];
+    let date = dateArg.getDate();
+    let day = days[dateArg.getDay()];
+
+    return `${date} ${month} (${day}), ${year}`;
 }
